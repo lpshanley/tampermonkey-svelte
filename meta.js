d477c8a148f6a9bce34b89eeb426ee46e8f36cb5
Rex Walters <653208+wrex@users.noreply.github.com>
2021-12-26 20:08:24 -0800

    use distURLBase correctly in meta.js
    Only want `file://` URLs with dev mode

--- meta.js
+++ meta.js
@@ -1,46 +1,37 @@
-const path = require('path');
-const { pathToFileURL } = require('url');
-const pkg = require('./package.json');
+const path = require("path");
+const { pathToFileURL, URL } = require("url");
+const pkg = require("./package.json");
 
 const distURLBase = `https://example.com/dist`;
 const packageName = pkg.name;
 
 const production = !process.env.ROLLUP_WATCH;
-const baseUrl = !production	? path.join(__dirname, 'dist') : distURLBase;
+const baseUrl = path.join(__dirname, "dist");
 
 let meta = {
-    "name": production ? packageName : packageName + ' -> dev',
-    "version": pkg.version,
-    "description": pkg.description,
-	"homepage": pkg.homepage,
-	"author": pkg.author,
-    "namespace": "https://github.com",
-    "resource": {
-		css: pathToFileURL(path.join(baseUrl, 'bundle.css'))
-	},
-    "match": [
-        "https://*.github.com/*"
-    ],
-    "grant": [
-        "GM_addStyle",
-        "GM_getResourceText",
-        "GM_xmlhttpRequest"
-    ],
-    "connect": [
-        "github.com"
-    ],
-    "run-at": "document-idle"
+  name: production ? packageName : packageName + " -> dev",
+  version: pkg.version,
+  description: pkg.description,
+  homepage: pkg.homepage,
+  author: pkg.author,
+  namespace: "https://github.com",
+  resource: {
+    css: pathToFileURL(path.join(baseUrl, "bundle.css")),
+  },
+  match: ["https://*.github.com/*"],
+  grant: ["GM_addStyle", "GM_getResourceText", "GM_xmlhttpRequest"],
+  connect: ["github.com"],
+  "run-at": "document-idle",
+};
+
+if (!production) {
+  meta.require = [pathToFileURL(path.join(baseUrl, "bundle.js"))];
 }
 
-if(!production){
-	meta.require= [
-        pathToFileURL(path.join(baseUrl, 'bundle.js'))
-    ];
-}
-
-if(production) {
-	meta.downloadURL = pathToFileURL(path.join(baseUrl, 'bundle.js'));
-	meta.updateURL = pathToFileURL(path.join(baseUrl, 'bundle.js'));
+if (production) {
+  meta.downloadURL = new URL("bundle.js", distURLBase);
+  meta.updateURL = new URL("bundle.js", distURLBase);
+  meta.resource.css = new URL("bundle.css", distURLBase);
 }
 
 module.exports = meta;
